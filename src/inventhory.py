bac340442db36db0131518d0108c63d893b78b70
Martina Pauer <martinapauer38@gmail.com>
2025-09-18 17:46:45 +0000

    FIX SQL QUERIES

--- src/inventhory.py
+++ src/inventhory.py
@@ -113,7 +113,7 @@ class Inventory():
         consult.write(f'CREATE TABLE {table_name} (good_name varchar(255), available_units int, price FLOAT(6, 2), currency varchar(3);')
         # Insert values from this inventory        
         for item in self.items:
-                consult.write(f'INSERT INTO {table_name} ({item.name}, {item.count}, {item.price[1]}, {item.price[0]})')
+                consult.write(f'INSERT INTO {table_name}(good_name, available_units, price, currency) VALUES ({item.name}, {item.count}, {item.price[1]}, {item.price[0]});')
         consult.close()
         # Load from the file and run in sqlite SQL manager
         self.load('info.sql')
