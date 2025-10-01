28be8ae947fd9a48d6bb295b9c4b506333145adf
Martina Pauer <martinapauer38@gmail.com>
2025-10-01 23:17:20 +0000

    Make Structure in two Files for Front-End
    Start now but let for later because is too late
    for make computer draft of the Interface

---
+++ src/manager.py
@@ -0,0 +1,8 @@
+class Tables():
+    def __init__(self, tab: str, window_size: tuple[int, int]):
+        '''
+            Make easier GTK window with tables to
+            show data, a very common kind of
+            programs for enterprises
+        '''
+        self.table: str = tab
