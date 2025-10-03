d09970d47817be65ccf65dc117160f81b46cb32e
Martina Pauer <martinapauer38@gmail.com>
2025-10-03 05:37:56 +0000

    All the Main Structure complete
    Only need complete details for:
    
        - Manager.details methods with Gtk Part
        - Inventory logic for thory

--- src/manager.py
+++ src/manager.py
@@ -1,8 +1,74 @@
-class Tables():
+import gi
+gi.require_version('Gtk', '3.0')
+import Gtk
+import Gdk
+
+class Tables(Gtk.window):
     def __init__(self, tab: str, window_size: tuple[int, int]):
         '''
             Make easier GTK window with tables to
             show data, a very common kind of
-            programs for enterprises
+            programs for enterprises:
+
+            Tables.table, text for the table
+
+            Tables.ins_container, block for inputs
+
+            Tables.outs_container, block for outputs
         '''
         self.table: str = tab
+        
+        self.ins_container = Gtk.VBox()
+
+        self.outs_container = Gtk.HBox()
+        
+    def set_inputs(self, inputs: list[str]):
+        '''
+            Give different text input
+            to the inputs container
+        '''
+        for input_data in inputs:
+            self.ins_container.add(
+                    Gtk.Entry
+                    (
+                        label = input_data
+                    )
+                )
+            
+    def last_outputs(self, outputs: list):
+        '''
+            Give text to display
+            in the end
+        '''
+        for output in outputs:
+            self.outs_container.add(
+                Gtk.Label(output)
+            )
+
+    def set_columns(self, names: list[str]):
+            '''
+                Define columns names
+            '''
+            for name in names:
+                 pass
+            
+    def parametric_input(self, parameters: list[str], property_name: str):
+         '''
+            Select paramater from menu
+            and give it value
+         '''            
+         pass   
+
+    def get_style(self, css_filename: str):
+        '''
+            Give custom style from
+            CSS file
+        '''    
+        pass
+
+    def run(self):
+         '''
+            Show the current graphical
+            interface
+         '''
+         pass
--- src/thory.py
+++ src/thory.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 from manager import Tables
+import inventhory
+
+app = Tables(inventhory.Inventory, (300, 300))
+# Define the data getting
+app.set_inputs(
+        [
+            'Product Name',
+            'Units In Stock'
+        ]
+)
+
+app.parametric_input(
+        ['ARS', 'USD', '€', '¥'],
+        'Price'    
+)
+# Give name to the columns
+app.set_columns(
+        [
+            'Date', 'Name',
+            'Units', 'Price'
+        ]
+    )
+# Define the text to show
+app.last_outputs(
+    # Show the price of all the stock
+    inventhory.Inventory.calc_price()[0],
+    inventhory.Inventory.calc_price()[1]
+)
+# Customize with own style
+app.get_style('gtk_theme.css')
+# Show the interface when get all
+app.run()
