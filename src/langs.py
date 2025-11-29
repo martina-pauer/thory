485a572b186401f8673a4d74a5f1f50face6b4b3
Martina Pauer <martinapauer38@gmail.com>
2025-11-29 20:13:25 +0000

    Reestructure Langs Infraestructure
    More Easy and seperate from the logic

---
+++ src/langs.py
@@ -0,0 +1,163 @@
+import traslator
+
+traslations = traslator.Traslator()
+# Spanish
+traslations.add_traslation  (
+                        'es', 'Stock', 
+                        'Inventario'
+                    )
+
+traslations.add_traslation  (
+                        'es', 'Product Name',
+                        'Producto'     
+                    )
+
+traslations.add_traslation  (
+                        'es', 'Units In Stock',
+                        'Unidades En Inventario'
+                    )  
+
+traslations.add_traslation    (
+                    'es', 'Send',
+                    'Enviar'
+                )
+
+traslations.add_traslation    (
+                    'es', 'Price',
+                    'Precio'
+                ) 
+
+traslations.add_traslation    (
+                    'es', 'Date', 
+                    'Fecha'
+                )
+
+traslations.add_traslation    (
+                    'es', 'Units',
+                    'Unidades'
+                )   
+
+traslations.add_traslation   (
+                    'es', 'Name',
+                    'Nombre'
+                )                
+# French
+traslations.add_traslation  (
+                        'fr', 'Stock', 
+                        'Inventaire'
+                    )
+
+traslations.add_traslation  (
+                        'fr', 'Product Name',
+                        'Produit'     
+                    )
+
+traslations.add_traslation  (
+                        'fr', 'Units In Stock',
+                        'Unités En Inventaire'
+                    )  
+
+traslations.add_traslation    (
+                    'fr', 'Send',
+                    'Envoyer'
+                )
+
+traslations.add_traslation (
+                    'fr', 'Price',
+                    'Prix'
+                ) 
+
+traslations.add_traslation    (
+                    'fr', 'Date', 
+                    'Date'
+                )
+
+traslations.add_traslation    (
+                    'fr', 'Units',
+                    'Unités'
+                )   
+
+traslations.add_traslation   (
+                    'fr', 'Name',
+                    'Nom'
+                )                
+# Deutsch (German)
+traslations.add_traslation  (
+                        'de', 'Stock', 
+                        'Inventar'
+                    )
+
+traslations.add_traslation  (
+                        'de', 'Product Name',
+                        'Produkt'     
+                    )
+
+traslations.add_traslation  (
+                        'de', 'Units In Stock',
+                        'Einheiten Im Bestand'
+                    )  
+
+traslations.add_traslation    (
+                    'de', 'Send',
+                    'Schicken'
+                )
+
+traslations.add_traslation    (
+                    'de', 'Price',
+                    'Preis'
+                ) 
+
+traslations.add_traslation    (
+                    'de', 'Date', 
+                    'Datum'
+                )
+
+traslations.add_traslation   (
+                    'de', 'Units',
+                    'Einheiten'
+                ) 
+
+traslations.add_traslation   (
+                    'de', 'Name',
+                    'Name'
+                )                
+# Italian
+traslations.add_traslation  (
+                        'it', 'Stock', 
+                        'Inventario'
+                    )
+
+traslations.add_traslation  (
+                        'it', 'Product Name',
+                        'Prodotto'     
+                    )
+
+traslations.add_traslation  (
+                        'it', 'Units In Stock',
+                        "Unità Nell' inventario"
+                    )  
+
+traslations.add_traslation    (
+                    'it', 'Send',
+                    'Inviare'
+                )
+
+traslations.add_traslation    (
+                    'it', 'Price',
+                    'Prezzo'
+                ) 
+
+traslations.add_traslation    (
+                        'it', 'Date', 
+                        'Data'
+                    )
+
+traslations.add_traslation   (
+                    'it', 'Units',
+                    'Unità'
+                ) 
+
+traslations.add_traslation   (
+                    'it', 'Name',
+                    'Nome'
+                )  
