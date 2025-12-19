ca3d2f7dc9165f049ef9bc2982bec77b278f4179
Martina Pauer <martinapauer38@gmail.com>
2025-12-19 15:32:49 +0000

    Add JS, Improve CSS and Fix HTML
    Github Pages haven't logic for server then add
    JavaScript programming for send to a future flask
    Back-End in Python

---
+++ src/sender.js
@@ -0,0 +1,13 @@
+function sendToServer()
+{
+    // Collect all the data from the form and send with some API to Python Back-End
+    // JSON values for the getted from the inputs
+    let data = {
+        "product" : document.getElementById("thory-product").value,
+        "units": document.getElementById("thory-units").value,
+        "currency": document.getElementById("thory-currency")
+
+    };
+    // Modify in the future WHEN KNOW HOW SEND TO PYTHON BACKEND
+    console.log(data);
+}
