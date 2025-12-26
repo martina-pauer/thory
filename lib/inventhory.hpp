811a3529b198c0fe9f9baf2ea6603e1bed5e5a34
Martina Pauer <martinapauer38@gmail.com>
2025-12-26 16:31:08 +0000

    End Good Class: Later continue with Inventhory

--- lib/inventhory.hpp
+++ lib/inventhory.hpp
@@ -6,26 +6,45 @@ class Money
             this -> currency = cur;
             this -> amount = price;
         }
+
+        float get_amount()
+        {
+            return this -> amount;
+        }
+
+        std :: string get_currency()
+        {
+            return this -> currency;
+        }    
     private:
-        std :: string currency;    
+        std :: string currency; 
+        float amount;   
 };
 
 class Good
 {
     public:
-        Good(std :: string naming, Money price)
+        Good(std :: string naming, Money price_object)
         {
             this -> name = naming;
-            this -> price = {price.currency price.amount};
+            this -> price = price_object;
             this -> count = 1;
         }
 
+        void set_count(int recount)
+        {
+            // Set new count of available units in stock
+            this -> count = recount;
+        }
+            
         void calc_price()
         {
+            this -> price = Money(this -> price.get_currency(), this -> price.get_amount() * this -> count);
         }
 
         void convert(float equivalence, std :: string currency)
         {
+            this -> price = Money(currency, this -> price.get_amount() * equivalence);
         }   
 
         std :: string get_name()
