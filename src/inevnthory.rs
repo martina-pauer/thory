e471d423c778f4a01120cb29210a6bea4cd42f78
Martina Pauer <martinapauer38@gmail.com>
2025-12-30 20:17:56 +0000

    Fixing step by step Rust code

--- src/inevnthory.rs
+++ src/inevnthory.rs
@@ -2,7 +2,7 @@ pub struct Good
 { 
     // Atributes
     name: &str,
-    price: Vec<&str, f32>,
+    price: (&str, f32),
     count: u32
 }
 
@@ -15,29 +15,34 @@ pub struct Inventhory
 // Methods
 impl Good
 {
-    pub fn calc_price(&self)
+    pub fn calc_price(&self) -> (&str, f32)
     {
-        self.price[1] *= (self.price[1] * self.count);
+      // Calc price by mult unitary price and unit in stock  
+        self.price.1 = (self.price.1 * self.count);
+    // Give the result as tuple 'currency' and 'price' only type multi-type
+        return self.price
     }
 
     pub fn convert(&self, equiv:f32, cur:&str)
     {
-
+      // Made the change from one currency to other for get equivalent price  
+        self.price.1 = self.price.1 * equiv;
+        self.price.2 = cur;
     }
 
-    pub fn get_name(&self)
+    pub fn get_name(&self) -> &str
     {
-
+        return self.name;
     }
 
-    pub fn get_price(&self)
+    pub fn get_price(&self) -> (&str, f32)
     {
-
+        return self.price;
     }
 
-    pub fn get_count(&self)
+    pub fn get_count(&self) -> u32
     {
-
+        return self.count;
     }
 }
 
@@ -53,17 +58,22 @@ impl Inventhory
 
     }
 
-    pub fn calc_price(&self)
+    pub fn calc_price(&self) -> (&str, f32)
     {
-
+        for item in self.items
+        {
+            item.calc_price();
+        }   
     }
 
-    pub fn save(&self)
+    pub fn save(&self) -> &str
     {
-
+       // Storage data in database and give table ass text 
+        let mut table = "";
+        return table;
     }
 
-    pub fn get_items(&self)
+    pub fn get_items(&self) -> Vec<Good>
     {
         return self.items;
     }
