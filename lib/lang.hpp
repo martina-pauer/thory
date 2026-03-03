03cf70c496caafa6fbca8fbaa94f9f30ac8741ee
Martina Pauer <martinapauer38@gmail.com>
2026-03-03 21:41:17 +0000

    Full Lang Implementation C++: it, de, es, en

--- lib/lang.hpp
+++ lib/lang.hpp
@@ -1,4 +1,4 @@
-class Dict()
+class Dict
 {
     private:
         std :: string lang_code;
@@ -62,3 +62,61 @@ class Traslator
     private:
         Dict[] words;
 };
+// Create Object
+Traslator translation = Traslator();
+// Define Words
+std :: string[14] init_words = 
+                {
+                    "Stock", "Product Name", "Units In Stock", "Send", "Price", 
+                    "Date", "Units", "Name", "Moment", "Good_Name", "Available_Units", 
+                    "Currency", "inventory_items", "Total_ARS_Price"
+                };
+
+std :: string[14] es_words =
+                {
+                    "Inventario", "Producto", "Unidades En Inventario", "Enviar", "Precio",
+                    "Fecha", "Unidades", "Nombre", "Momento", "Producto", "Unidades_Disponibles",
+                    "Divisa", "inventario", "Precio_Total_Pesos_Argentinos"
+                };
+
+std :: string[14] fr_words =
+                {
+                    "Inventaire", "Produit", "Unités en Inventaire", "Envoyer", "Prix",
+                    "Date", "Unités", "Nom", "Moment", "Nom_Des_Merchandise",
+                    "Unites_Disponibles", "Devise", "Inventaire", "Prix_Total_Peso_Aregntin"
+                };
+
+std :: string[14] de_words =
+                {
+                    "Inventar", "Produkt", "Einheiten Im Bestand", "Schicken", "Preis",
+                    "Data", "Einheiten", "Name", "Moment", "Produkt", "Verfugbare_Einheiten",
+                    "Wahrung", "Inventar", "Gesamt-Aregentinisher-Preis"
+                };
+
+std :: string[14] it_words =
+                {
+                    "Inventario", "Prodotto", "Unita Nell' Inventario", "Inviare", "Prezzo",
+                    "Data", "Unita", "Nome", "Momento", "Prodotto", "Unita_Disponibili",
+                    "Valuta", "Inventario", "Prezo_Totale_ARS"
+                };
+// Setup Translations
+for (unsigned int word_index = 0; word_index < 14; word_index++)
+{ // spanish
+    translation.add_translations("es", init_words[word_index], es_words[word_index])
+}
+
+for (unsigned int word_index = 0; word_index < 14; word_index++)
+{
+    translation.add_translations("fr", init_words[word_index], fr_words[word_index])
+}
+
+for (unsigned int word_index = 0; word_index < 14; word_index++)
+{
+    translation.add_translations("de", init_words[word_index], de_words[word_index])
+}
+
+for (unsigned int word_index = 0; word_index < 14; word_index++)
+{
+    translation.add_translations("it", init_words[word_index], it_words[word_index])
+}
+// Now only use translation.translate method for get the text
